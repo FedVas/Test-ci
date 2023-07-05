3232d8ca50584f7698a28120ab2685eb8c588be7
Fedor <atlz89@mail.ru>
2023-07-05 14:59:42 +0300

    Add code

---
+++ src/main/java/trhyrth.java
@@ -0,0 +1,35 @@
+public class trhyrth {
+    int a = 3;
+    int d = 6;
+    int i = 4;
+
+    public trhyrth(int a, int d, int i) {
+        this.a = a;
+        this.d = d;
+        this.i = i;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
